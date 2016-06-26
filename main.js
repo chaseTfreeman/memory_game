07532d8f74ad7c8b7441c19c87d964ba601c5db2
Chase <chasefreeman13@gmail.com>
2016-06-26 18:52:55 -0400

    add lesson 10

--- main.js
+++ main.js
@@ -3,7 +3,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardOne === cardTwo) {
 	alert('You found a match!');	
 } else {
   alert('Sorry, try again.');
+}
+*/
+
+window.onload = function() {
+var getBoard = document.getElementById('game-board');
+ for(var i=0; i < 4; i++){
+   var newCard = document.createElement('div').appendChild('game-board');
+   getBoard.className = 'cards';
+}
+
+
+
